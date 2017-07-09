80204a1e948bf6d74828bcfc716765862bddef7b
Disa0808 <levshukdenis@mail.ru>
2017-07-09 21:26:37 +0300

    Keep values on post

--- app.rb
+++ app.rb
@@ -4,16 +4,18 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+
+
 set :database, "sqlite3:barbershop.db"
+
 
 class Client < ActiveRecord::Base
 	validates :name, presence: true
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true	
-
-
 end
+
 
 class Barber < ActiveRecord::Base
 end
@@ -23,24 +25,24 @@ before do
 	@barbers = Barber.order "created_at DESC"
 end
 
+
 get '/' do
-
-
 	erb :index	
-
 end
 
+
 get '/visit' do
+	@c = Client.new
 	erb :visit
 end
 
+
 post '/visit' do
-
-	c = Client.new params[:client]
-	c.save
-
-
-
-	erb "<h2>Спасибо, вы записались</h2>"
-
+	@c = Client.new params[:client]
+	if @c.save
+		erb "<h2>Спасибо, вы записались</h2>"
+	else
+		@error = @c.errors.full_messages.first
+		erb :visit
+	end
 end
