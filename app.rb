06dc6c4a4aa1020323e6499115bd8ec0f5391eac
Disa0808 <levshukdenis@mail.ru>
2017-07-09 23:51:12 +0300

    Add barber page stub

--- app.rb
+++ app.rb
@@ -10,7 +10,7 @@ set :database, "sqlite3:barbershop.db"
 
 
 class Client < ActiveRecord::Base
-	validates :name, presence: true
+	validates :name, presence: true, length: { minimum: 3 }
 	validates :phone, presence: true
 	validates :datestamp, presence: true
 	validates :color, presence: true	
@@ -45,4 +45,9 @@ post '/visit' do
 		@error = @c.errors.full_messages.first
 		erb :visit
 	end
+end
+
+
+get '/barber/:id' do
+	erb :barber
 end
